bc065fb82c599a5da5352ed8fa9712e4f0c53b1f
leon.zhou <leon.zhou@vendhq.com>
2020-01-13 11:53:26 +1300

    update

--- src/a_component/Loading/index.js
+++ src/a_component/Loading/index.js
@@ -1,4 +1,3 @@
-/** Loading组件 用于按需加载时过渡显示等 **/
 import React from "react";
 import "./index.less";
 import ImgLoading from "../../assets/loading.gif";
@@ -13,11 +12,11 @@ export default class LoadingComponent extends React.PureComponent {
   makeType(p) {
     let msg;
     if (p.error) {
-      msg = "加载出错，请刷新页面";
+      msg = "Load error,please refresh the page";
     } else if (p.timedOut) {
-      msg = "加载超时";
+      msg = "Load timeout";
     } else if (p.pastDelay) {
-      msg = "加载中…";
+      msg = "Loading…";
     }
     return msg;
   }
